5c67c9cd67dc63783c5411c32573743b8df75d97
Jing-Ting Dong <stevendong2002@gmail.com>
2023-09-28 11:19:13 -0400

    moved css to bottom

--- src/my-card.js
+++ src/my-card.js
@@ -7,7 +7,29 @@ class MyCard extends LitElement {
     header: { type: String },
   };
 
-
+  render() {
+    return html`
+      <div class="cardcontainer" id="cardcontainer">
+        <div class="card" id="card" style="background-color: pink">
+          <!-- header -->
+          <h2 id="heading">Jing-Ting Dong at the Library</h2>
+          <!-- img -->
+          <img src="https://i.imgur.com/XPC2j0i.jpg" alt="Card Image">
+          <div class="card-content">
+            <!-- body paragraph -->
+            <p id="paragraph">Hi my name is Jing-Ting Dong. This is an image of me giving a thumbs up at the library while working on this coding project. I like cabbage. I also like bacon. I really like eating but it gives me food coma. Bygolly, what a dilemma.</p>  
+            <!-- hide paragraph button -->
+            <button class="button" id="hideButton">Details</button>
+          </div>
+        </div>
+      </div>
+      <!-- Buttons -->
+      <button id="duplicateButton">Duplicate Card</button>
+      <button id="toggleButton">Toggle Background</button>
+      <button id="changeHeadingButton">Change Heading</button>
+      <button id="removeDuplicate">Remove A Card</button>
+    `;
+}
 
   connectedCallback() {
     super.connectedCallback();
@@ -79,6 +101,7 @@ class MyCard extends LitElement {
       cardContainer.removeChild(lastCard);
     }
   }
+
   static styles = css`
   :host {
     min-height: 100vh;
@@ -155,29 +178,4 @@ render() {
     </p>
   `;
 }
-
-render() {
-  return html`
-    <div class="cardcontainer" id="cardcontainer">
-      <div class="card" id="card" style="background-color: pink">
-        <!-- header -->
-        <h2 id="heading">Jing-Ting Dong at the Library</h2>
-        <!-- img -->
-        <img src="https://i.imgur.com/XPC2j0i.jpg" alt="Card Image">
-        <div class="card-content">
-          <!-- body paragraph -->
-          <p id="paragraph">Hi my name is Jing-Ting Dong. This is an image of me giving a thumbs up at the library while working on this coding project. I like cabbage. I also like bacon. I really like eating but it gives me food coma. Bygolly, what a dilemma.</p>  
-          <!-- hide paragraph button -->
-          <button class="button" id="hideButton">Details</button>
-        </div>
-      </div>
-    </div>
-    <!-- Buttons -->
-    <button id="duplicateButton">Duplicate Card</button>
-    <button id="toggleButton">Toggle Background</button>
-    <button id="changeHeadingButton">Change Heading</button>
-    <button id="removeDuplicate">Remove A Card</button>
-  `;
-}
-
 };
