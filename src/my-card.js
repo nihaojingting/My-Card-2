35e4b923e68a911654e2ae40525de31dcb2c7449
Jing-Ting Dong <stevendong2002@gmail.com>
2023-10-03 11:32:22 -0400

    changes

--- src/my-card.js
+++ src/my-card.js
@@ -12,32 +12,7 @@ class MyCard extends LitElement {
     this.header = 'My app';
   }
 
-  render() {
-    return html`
-      <div class="cardcontainer" id="cardcontainer">
-        <div class="card" id="card" style="background-color: pink">
-          <!-- header -->
-          <h2 id="heading">Jing-Ting Dong at the Library</h2>
-          <!-- img -->
-          <img src="https://i.imgur.com/XPC2j0i.jpg" alt="Card Image">
-          <div class="card-content">
-            <!-- body paragraph -->
-            <details>
-  <summary>Summary</summary>
-  <p id="paragraph">Hi my name is Jing-Ting Dong. This is an image of me giving a thumbs up at the library while working on this coding project. I like cabbage.</p>
-</details>
-          </div>
-        </div>
-      </div>
-      <!-- Buttons -->
-      <button id="duplicateButton" @click=${this.duplicateButton}>Duplicate Card</button>
-      <button id="toggleButton" @click=${this.toggleButton}>Toggle Background</button>
-      <button id="changeHeadingButton" @click=${this.changeHeadingButton}>Change Heading</button>
-      <button id="removeDuplicate" @click=${this.removeDuplicate}>Remove A Card</button>
-    `;
-}
-
-static styles = css`
+  static styles = css`
   :host {
     min-height: 100vh;
     display: flex;
@@ -79,6 +54,31 @@ static styles = css`
     margin-left: 5px;
   }
 `;
+
+  render() {
+    return html`
+      <div class="cardcontainer" id="cardcontainer">
+        <div class="card" id="card" style="background-color: pink">
+          <!-- header -->
+          <h2 id="heading">Jing-Ting Dong at the Library</h2>
+          <!-- img -->
+          <img src="https://i.imgur.com/XPC2j0i.jpg" alt="Card Image">
+          <div class="card-content">
+            <!-- body paragraph -->
+            <details>
+  <summary>Summary</summary>
+  <p id="paragraph">Hi my name is Jing-Ting Dong. This is an image of me giving a thumbs up at the library while working on this coding project. I like cabbage.</p>
+</details>
+          </div>
+        </div>
+      </div>
+      <!-- Buttons -->
+      <button id="duplicateButton" @click=${this.duplicateButton}>Duplicate Card</button>
+      <button id="toggleButton" @click=${this.toggleButton}>Toggle Background</button>
+      <button id="changeHeadingButton" @click=${this.changeHeadingButton}>Change Heading</button>
+      <button id="removeDuplicate" @click=${this.removeDuplicate}>Remove A Card</button>
+    `;
+}
 
 duplicateButton() {
   //THE DUPLICATING BUTTON STUFF
